c520a3cb43096d929cebf85163be31b661aba488
Leonardo Barcaroli <leo.barcaroli@gmail.com>
2022-11-25 22:24:27 +0100

    fmt

--- password_test.go
+++ password_test.go
@@ -1,43 +1,43 @@
 package userz
 
 import (
-    "bytes"
-    "io/ioutil"
-    "encoding/json"
-    "testing"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
-    "github.com/stretchr/testify/assert"
 )
 
 const testPass = "ciaomiaobau"
 
 type testVessel struct {
-    Password *Password `json:"password"`
+	Password *Password `json:"password"`
 }
 
 func TestPassword(t *testing.T) {
-    assert := assert.New(t)
+	assert := assert.New(t)
 
-    b := bytes.NewBuffer(nil)
-    enc := json.NewEncoder(b)
+	b := bytes.NewBuffer(nil)
+	enc := json.NewEncoder(b)
 
-    pass, err := NewPassword(testPass)
-    assert.NoError(err)
+	pass, err := NewPassword(testPass)
+	assert.NoError(err)
 
-    err = enc.Encode(&testVessel{
-        Password: pass,
-    })
-    assert.NoError(err)
+	err = enc.Encode(&testVessel{
+		Password: pass,
+	})
+	assert.NoError(err)
 
-    w, err := ioutil.ReadAll(b)
-    assert.NoError(err)
+	w, err := ioutil.ReadAll(b)
+	assert.NoError(err)
 
-    var newVessel testVessel
+	var newVessel testVessel
 
-    err = json.Unmarshal([]byte(w), &newVessel)
-    assert.NoError(err)
+	err = json.Unmarshal([]byte(w), &newVessel)
+	assert.NoError(err)
 
-    err = bcrypt.CompareHashAndPassword([]byte(*newVessel.Password), []byte(testPass))
-    assert.NoError(err)
+	err = bcrypt.CompareHashAndPassword([]byte(*newVessel.Password), []byte(testPass))
+	assert.NoError(err)
 }
